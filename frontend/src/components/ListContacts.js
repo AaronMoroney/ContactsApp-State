a4609e071042c4b235e678bb09fe8beba722c159
aaron moroney <aaron.createdesign@gmail.com>
2023-04-12 13:08:53 +0100

    mapped over the contacts list, to produce a list of names

--- frontend/src/components/ListContacts.js
+++ frontend/src/components/ListContacts.js
@@ -2,10 +2,13 @@ const ListContacts = ({ contacts }) => {
     console.log(contacts);
     return (
         <ol className = 'contact-list'>
-
+           {
+                contacts.map((contact) => (
+                    <li key={contact.id}>{contact.name}</li>
+                ))
+           }
         </ol>
-    )
-
-}
+    );
+};
 
 export default ListContacts;
