06cfbf0afe574b6134e2d6a4148d34b214deeb53
aaron moroney <aaron.createdesign@gmail.com>
2023-04-20 15:09:47 +0100

    showing number of contacts functionality

--- frontend/src/components/ListContacts.js
+++ frontend/src/components/ListContacts.js
@@ -1,12 +1,18 @@
 import PropTypes from 'prop-types'
 import { useState } from 'react'
 
+
 const ListContacts = ({ contacts, onDeleteContact }) => {
     const [query, setQuery] = useState("");
     const updateQuery = (query) => {
         //.trim () = remove whitespace
         setQuery(query.trim());
     }
+
+    const clearQuery = () => {
+        updateQuery("")
+    }
+
 
     const showingContact = query === "" 
     ? contacts 
@@ -25,6 +31,14 @@ const ListContacts = ({ contacts, onDeleteContact }) => {
                 onChange={(event) => updateQuery(event.target.value)}
                 ></input>
             </div>
+            {
+                showingContact.length !== contacts.length && (
+                    <div className='showing-contacts'>
+                        <span> Now showing {showingContact.length} of {contacts.length}</span>
+                        <button  onClick={clearQuery}>Show all</button>
+                    </div>
+                )
+            }
              <ol className = 'contact-list'>
             {
                 showingContact.map((contact) => (
